89d0a378c668a519726df2ffcc2b9bf87df301fa
KinMiu <115400530+KinMiu@users.noreply.github.com>
2023-09-15 00:12:50 +0700

    update cors

--- src/app.js
+++ src/app.js
@@ -14,7 +14,12 @@ mongo.createConnection().then((_) => {
 
 const app = express()
 
-app.use(cors())
+const corsOptions = {
+    origin: "https://photo-frontend-3qj3.vercel.app",
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
