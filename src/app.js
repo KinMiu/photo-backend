ca1bde81d9ef595041bcac552f5648ccc6ea0bbe
KinMiu <115400530+KinMiu@users.noreply.github.com>
2023-12-29 15:54:31 +0700

    update 29 des 2023

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ mongo.createConnection().then((_) => {
 
 const app = express()
 
-app.use(cors)
+app.use(cors())
 app.use(cookies())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
