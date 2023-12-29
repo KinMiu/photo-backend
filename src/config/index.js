a520d5c993154d1fa1d73f3f3fea89559762e25c
KinMiu <115400530+KinMiu@users.noreply.github.com>
2023-12-29 17:39:03 +0700

    update 29 des 2023

--- src/config/index.js
+++ src/config/index.js
@@ -17,12 +17,18 @@ if (process.env.NODE_DEV === 'development') {
 
 const cors = (req, res, next) => {
     const requestHost = req.get('origin') || req.get('host')
+
     if (allowedOrigins.some((origin) => requestHost.match(origin))) {
         res.header('Access-Control-Allow-Origin', req.get('origin'))
         res.header('Access-Control-Allow-Credentials', 'true')
     }
+
     res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization")
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
     next()
 }
 
